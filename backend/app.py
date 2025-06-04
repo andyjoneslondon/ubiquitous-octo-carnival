0626545177da4d38f5ac8ac0fdc62f15ee8aa45a
andyjoneslondon <Andy.jones.london@gmail.com>
2025-06-04 07:45:15 +0100

    Update app.py
    Updating CORS to fix errors

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from tts import generate_tts
 from db import save_report, get_latest_status
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://ubiquitous-octo-carnival.onrender.com"}}, supports_credentials=True)
+CORS(app, supports_credentials=True, origins=["https://ubiquitous-octo-carnival.onrender.com"], allow_headers=["Content-Type"], methods=["GET", "POST", "OPTIONS"])
 
 @app.route('/process_text', methods=['POST'])
 def process_text():
